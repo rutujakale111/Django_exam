f8b10aa32f24b30cdbbf9361757e2d394b83fb1f
rutujakale111 <rutujak1105@gmail.com>
2024-11-22 19:09:39 +0530

    first commit

---
+++ BasicExam/teachers/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Teacher
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'subject', 'email')
